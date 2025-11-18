78d3e286e4646bdd129ee4c62125f15f754aa6e6
animeshsdev <s.animesh.dev@gmail.com>
2025-11-17 20:48:54 -0500

    dev: Simple Resume Design

--- src/assets/Views/MainApp.tsx
+++ src/assets/Views/MainApp.tsx
@@ -1,13 +1,13 @@
 //import PotteryPortfolio from './PotteryPortfolio/Portfolio';
 // import Blog from './Blog/Blog';
-import SimpleResume from './SimpleResume/SimpleResume';
-
+//import SimpleResume from './SimpleResume/SimpleResume';
+import Practise from './Practise/Practise';
 import ErrorBoundary from '../Components/ErrorBoundary/ErrorBoundary';
 
 const MainApp: React.FC<unknown> = () => {
     return (
         <ErrorBoundary>
-            <SimpleResume />
+            <Practise />
         </ErrorBoundary>
     )
 }
